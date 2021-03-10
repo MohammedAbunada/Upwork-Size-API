77c049c7d730f1240c5a44d9e45f8014726a3e7b
Mohammed Abunada <m.abunada@msn.com>
2021-03-10 22:27:12 +0300

    first commit

---
+++ django-docker-compose/SizeAPI/main/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse, JsonResponse
+import numpy as np
+
+import pickle
+
+NEEDED_KEY = "4KN4EWQ-983M1HX-PEFRVQC-Q6D1VJH"
+
+def get_size(weight, height, age):
+    bmi = 100*(weight / (height**2))
+    if bmi > 0.4 and weight > 65:
+        return "XXL"
+    elif bmi > 0.35:
+        return "XL"
+    model = pickle.load(open("model.pkl", "rb"))
+    return model.predict([[height,weight,age,bmi]])[0]
+
+SIZE_NAMES = {
+    "XS": "Extra Small",
+    "S": "Small",
+    "M":"Medium",
+    "L": "Large",
+    "XL": "Extra Large"
+}
+
+def size(request):
+    try:
+        needed_inputs = ["apiKey", "height", "weight", "age", "userId"]
+        inps = dict(request.GET)
+        inps = {i:j[0] if isinstance(j, list) else j for i,j in inps.items()}
+        missing = [i for i in needed_inputs if i not in inps]
+        if missing:
+            return JsonResponse({"status":"Failure", "body":f"The following inputs are missing! {needed_inputs}"})
+
+        s = get_size(float(inps['weight']), float(inps['height']), float(inps['age']))
+        return JsonResponse({"status":"Success", "size":s})
+    except:
+        return JsonResponse({"status":"Failure", "body":f"An unexpected error occured."})
